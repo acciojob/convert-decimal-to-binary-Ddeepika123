945230b57a2ad80e50c0b555e834622b24f18903
Dpka Singh <112195696+Ddeepika123@users.noreply.github.com>
2026-09-05 01:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function decimalToBinary(num) {
   // your code here
+	if (num === 0) {
+        return "0";
+    }
+
+    let binary = "";
+
+    while (num > 0) {
+        let remainder = num % 2;
+
+        binary = remainder + binary;
+
+        num = Math.floor(num / 2);
+    }
+
+    return binary;
 }
-const num = parseInt(prompt("Enter a number."));
-alert(decimalToBinary(num));
+//const num = parseInt(prompt("Enter a number."));
+//alert(decimalToBinary(num));
